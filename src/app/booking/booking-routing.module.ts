d40618fbc4381a00dd462f9ea1102bccc4f0cc75
Amr <amrraafat17@yahoo.com>
2023-12-28 14:42:57 +0200

    ...

--- src/app/booking/booking-routing.module.ts
+++ src/app/booking/booking-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BookingComponent } from './booking.component';
 import { BookingGuard } from './guards/booking.guard';
 
-
+//remove canDeactivate Route if needed, since it blocks the routing if the return value is false
 const routes: Routes = [{ path: '', component: BookingComponent, canDeactivate:[BookingGuard] }];
 
 @NgModule({
